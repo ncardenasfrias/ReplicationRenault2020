9c857dbfaf7b00bc5fa8de3a53b5043b3a36a91d
ncardenasfrias <cardenasfrias.natalia@gmail.com>
2024-04-26 07:14:24 +0200

    up nqive ngrams

--- CODE/4.Ngrams_NaiveBayes.py
+++ CODE/4.Ngrams_NaiveBayes.py
@@ -13,6 +13,13 @@ from datetime import datetime
 #Mongo DB 
 import pymongo 
 from pymongo.mongo_client import MongoClient
+#sklearn
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, matthews_corrcoef
+from sklearn.feature_extraction.text import CountVectorizer
+#balanced sample 
+from imblearn.under_sampling import RandomUnderSampler
 
 os.chdir("C:/Users/ncardenafria/Documents/GitHub/ReplicationRenault2020/")
 
@@ -30,7 +37,13 @@ try:
 except Exception as e:
     print(e)
     
+#%% call the database 
 db = client["StockTwits"]
-#call only what is needed, else it is going to be super heavy to load 
-df = pd.DataFrame(db["twits"].find({}))
+df = pd.DataFrame(list(db['twits_v2'].find({"sentiment": {"$ne": ""}, "n_three": 1, "is_bot": 0}, {"c_content": 1, "sentiment": 1})))
 
+
+#%% Get balanced data set, sample =250k
+sampler = RandomUnderSampler(sampling_strategy={-1: 125000, 1: 125000})
+X_bal, y_bal = sampler.fit_resample(df['c_content'].values.reshape(-1, 1),df["sentiment"])
+
+#%% use different types of 
