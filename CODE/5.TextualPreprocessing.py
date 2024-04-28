a3bc3dd6faa3120cd56d069c0644f104483f88dd
miguelgreengot <140072171+miguelgreengot@users.noreply.github.com>
2024-04-28 14:34:41 +0200

    Update 5.TextualPreprocessing.py

--- CODE/5.TextualPreprocessing.py
+++ CODE/5.TextualPreprocessing.py
@@ -5,59 +5,46 @@ Machine Learning - Other Algorithms
 @author: ncardenafria
 """
 
-#Basic packages 
-import pandas as pd
-import os
-import time 
-from datetime import datetime
-#Mongo DB 
-import pymongo 
-from pymongo.mongo_client import MongoClient
-
-os.chdir("C:/Users/ncardenafria/Documents/GitHub/ReplicationRenault2020/")
-
-#%% Connect to MongoDB and call the DB
-#mongodb password stocktwits
+# Connect to MongoDB
 uri = "mongodb+srv://nataliacardenas:stocktwits@twits.mgv4dfh.mongodb.net/?retryWrites=true&w=majority&appName=Twits"
-
-# Create a new client and connect to the server
 client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-    
 db = client["StockTwits"]
-#call only what is needed, else it is going to be super heavy to load 
-df = pd.DataFrame(db["twits"].find({}))
+df = pd.DataFrame(list(db["twits"].find({"sentiment": {"$ne": ""}}, {"content": 1, "sentiment": 1})))
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
 
+def apply_pos_tags(text):
+    words = word_tokenize(text)
+    pos_tags = pos_tag(words)
+    return ' '.join([f"{word}_{tag}" for word, tag in pos_tags])
 
-#%% Benchmark
+def emojis_and_punctuation(text):
+    text = emoji.demojize(text)
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
 
+# Function to preprocess, train, and evaluate
+def preprocess_train_evaluate(preprocess_fn, df, vectorizer, model):
+    df['processed_content'] = df['content'].apply(preprocess_fn)
+    X = vectorizer.fit_transform(df['processed_content'])
+    y = df['sentiment']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scores = cross_validate(model, X_train, y_train, scoring=['accuracy', 'matthews_corrcoef'], cv=10)
+    accuracy = scores['test_accuracy'].mean()
+    mcc = scores['test_matthews_corrcoef'].mean()
+    return accuracy, mcc
 
+vect = CountVectorizer(stop_words="english") 
+bayes = MultinomialNB()
 
-#%% Punctuation
+results = {}
+results['Benchmark'] = preprocess_train_evaluate(lambda x: x, df, vect, bayes)
+results['Punctuation'] = preprocess_train_evaluate(remove_punctuation, df, vect, bayes)
+results['Stem'] = preprocess_train_evaluate(stem_text, df, vect, bayes)
+results['Emoticons'] = preprocess_train_evaluate(handle_emojis, df, vect, bayes)
+results['StopWords'] = preprocess_train_evaluate(remove_stopwords, df, vect, bayes)
+results['PosTagging'] = preprocess_train_evaluate(apply_pos_tags, df, vect, bayes)
+results['Emojis + Punctuation'] = preprocess_train_evaluate(emojis_and_punctuation, df, vect, bayes)
 
-
-
-#%% Stem
-
-
-
-#%% Emoticons
-
-
-
-#%% StopWords
-
-
-
-#%% PosTag
-
-
-
-#%% Emojis + punctuation
-
+results_df = pd.DataFrame(results, index=['Accuracy', 'MCC']).T
+print(results_df)
